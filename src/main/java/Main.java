5ff793ee5ab43b3318f5e04e0e16f166f8d2d58e
Pawel Maliszewski <pablo999455@gmail.com>
2023-05-09 19:57:49 +0200

    problem z drugim testem, przechodzi kiedy robię go indywidualnie screen shot w slack

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 public class Main {
+    static Map<Integer, Number> numbers = new TreeMap<>();
 
+    public static void main(String[] args) throws FileNotFoundException {
 
-    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(new File("liczby.txt"))
+        ) {
+            while (sc.hasNextLine()) {
+                int number = sc.nextInt();
+                if (numbers.containsKey(number)) {
+                    int repetitionOfNumber = numbers.get(number).getQuantityOfNumber() + 1;
+                    numbers.put(number, new Number(number, repetitionOfNumber));
+                } else {
+                    numbers.put(number, new Number(number, 1));
+                }
+            }
+        }
 
+        Collection<Number> numbers = Main.numbers.values();
+        for (Number number : numbers) {
+            System.out.println(number);
+        }
     }
-
 }
