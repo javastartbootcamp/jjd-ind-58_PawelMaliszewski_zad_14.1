df8430cc82bd37262482a8e49f413d6411415f9f
Pawel Maliszewski <pablo999455@gmail.com>
2023-05-10 08:11:51 +0200

    v2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,28 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
-    static Map<Integer, Number> numbers = new TreeMap<>();
 
     public static void main(String[] args) throws FileNotFoundException {
 
-        try (Scanner sc = new Scanner(new File("liczby.txt"))
+        Map<Integer, Integer> numbers = new TreeMap<>();
+        //String fileName = "src\\main\\java\\liczby.txt"; // z tym plikiem test 1 i 3 nie przechodzi
+        String fileName = "liczby.txt";
+        try (Scanner sc = new Scanner(new File(fileName))
         ) {
-            while (sc.hasNextLine()) {
+            while (sc.hasNextInt()) {
                 int number = sc.nextInt();
                 if (numbers.containsKey(number)) {
-                    int repetitionOfNumber = numbers.get(number).getQuantityOfNumber() + 1;
-                    numbers.put(number, new Number(number, repetitionOfNumber));
+                    int repetitionOfNumber = numbers.get(number) + 1;
+                    numbers.put(number, repetitionOfNumber);
                 } else {
-                    numbers.put(number, new Number(number, 1));
+                    numbers.put(number, 1);
                 }
             }
         }
-
-        Collection<Number> numbers = Main.numbers.values();
-        for (Number number : numbers) {
-            System.out.println(number);
+        Collection<Integer> values = numbers.values();
+        Set<Integer> integers = numbers.keySet();
+        for (Integer integer : integers) {
+            System.out.println(integer + " - liczba wystąpień " + numbers.get(integer));
         }
     }
 }
